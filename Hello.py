c7c71086c466127a6df4bc09d0c8c5f7bab5bda8
laxman <laxman.uppada99@gmail.com>
2021-08-14 16:25:56 +0530

    First commit for Python program

---
+++ Hello.py
@@ -0,0 +1,6 @@
+# This is my First Python Program
+print (" Hello")
+print ( "******************************")
+print ( "Welcome to Python world")
+print ( "Good Luck for the Next exercise")
+print ( "******************************")
