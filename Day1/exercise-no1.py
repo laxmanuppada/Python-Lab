cdafa5df060a0d58f834b71ef8cc027d7258e050
laxman <laxman.uppada99@gmail.com>
2021-08-14 22:48:40 +0530

    string concatenation

--- Day1/exercise-no1.py
+++
@@ -1,3 +0,0 @@
-print ( "Day-1 - python print function")
-print ( "The function is declrated like this:")
-print ( "print('what to print')")
---
+++ Day1/string-manipulation.py
@@ -0,0 +1,6 @@
+# To string concatenation -Check the below syntax !
+print ( "Hello" "laxman")
+# To provide space between two words ! use below syntax
+print ( "Hello" + " " + "laxman")
+# one more options for getting sapce between two words by providing the space infront of quotes before string-laxman !
+print ("Hello"  "  Laxman")
