2016554e81b373cab97c28ac2289cd8390d4a63c
laxman <laxman.uppada99@gmail.com>
2021-08-15 14:22:57 +0530

    Day-1.3-input-exercise

---
+++ Day1/Day-1.3-input-exercise.py
@@ -0,0 +1,6 @@
+# input ("what is your name ?  ")
+# len ("laxman")
+# how to get length of a string ? 
+# print ( len ("laxman")) -- then it will print only one name , what if i want any other names ? 
+print  ( len (input ( "what is your name ? " ) ) )
+
