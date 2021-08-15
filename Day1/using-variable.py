e1cc3d77d1002d62754911752833eff94fddb567
laxman <laxman.uppada99@gmail.com>
2021-08-15 14:52:32 +0530

    using-variable

---
+++ Day1/using-variable.py
@@ -0,0 +1,15 @@
+# input ("what is your name ?  " )
+# Variable gives a name to piece of Data or Name given to some value or infromation !! 
+name = ("laxman")
+print (name)
+
+name = ("Raju")
+print (name) 
+
+print ( len ( input ("what is your name ? " ) ) )
+
+# For above code i want to use variable
+
+name = input ("what is your name ? " )
+length = len (name)
+print (length)
