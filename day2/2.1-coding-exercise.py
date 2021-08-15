c85b71b4361610328c728dbc7adcca422c76bf70
laxman <laxman.uppada99@gmail.com>
2021-08-16 02:52:11 +0530

    2.1-coding-exercise

---
+++ day2/2.1-coding-exercise.py
@@ -0,0 +1,12 @@
+two_digit_number = input("type two didgit number")
+first_digit = two_digit_number [0]
+second_digit = two_digit_number [1]
+
+print (first_digit)
+print (second_digit)
+
+results = first_digit + second_digit
+print (results)
+
+results = int(first_digit) + int(second_digit)
+print (results)
