335e5c956bb0d2654abcfb5b9c6843b8bec67078
laxman <laxman.uppada99@gmail.com>
2021-08-15 13:51:49 +0530

    using input function

---
+++ Day1/input.py
@@ -0,0 +1,6 @@
+# input ("what is your name")
+# print ("what is your name ")
+# input() will get user inputs in the console 
+# Then print () will print the word "Hello" and the user inputs.
+# below code we are using concatenation and input function to get our required code.
+print ("Hello " + input ("what is your name?"))
