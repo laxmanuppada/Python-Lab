42e67664056db583faa1d40fe248e0cf04719f63
laxman <laxman.uppada99@gmail.com>
2021-08-15 15:28:39 +0530

    Project-Brand-name-generator

---
+++ Day1/Project-Brand-name-generator.py
@@ -0,0 +1,12 @@
+# create a greetings for the new program !
+print ("welcome to Band name Generator. ")
+# Ask the user for the city that they Grew up in ? 
+# input ("which city did you grow up in ? " ) -- To make presistant we will use Variables here !
+city = input (" which city did you group up in ?\n " )
+# Ask user for the name of the pet ? 
+# input ("what is the name of the pet " ) -- we will use variables here !! 
+pet = input ( " what is the name of the pet\n " )
+# combine the name of their city and pet an show them their brand name. 
+print (" Your Band name could be " + city + " " + pet )
+# Note : Make sure the inputs cursor shows on a new line for this we need to add \n for the poin no 5 and 6 
+
