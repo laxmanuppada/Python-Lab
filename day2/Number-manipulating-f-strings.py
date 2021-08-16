e5ac3d7449cd9d7c389e47f3dc7bf83c879eba10
laxman <laxman.uppada99@gmail.com>
2021-08-17 00:49:12 +0530

    Number-manipulating-f-strings

---
+++ day2/Number-manipulating-f-strings.py
@@ -0,0 +1,34 @@
+print (8 / 3)
+# To round off the decimal number
+print (round (8 / 3))
+
+print (round (8 /3, 2))
+#  or 
+print ( 8 // 3 )
+
+# To check the Data type for the below code 
+print (type(8/3))
+print (type(8//3))
+
+result = 4/2
+result /=2
+print (result)
+
+score = 0
+# use score as a point 
+
+# score = score + 1
+score += 1
+print (score)
+
+score -= 1
+print(score)
+
+score = 0
+# print ("your score is " + str(score))
+# F-string - to convert all types of data types in to intger.
+height = 1.8
+iswinning = True
+# f-string
+print(f"your score is {score}\n your height is {height}\n your winning is {iswinning}")
+
