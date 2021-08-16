b80693fadaaab00217f103cbbe1d30c7f066fc09
laxman <laxman.uppada99@gmail.com>
2021-08-17 03:10:58 +0530

    Controll-flow-if-else

---
+++ Day3/Controll-flow-if-else.py
@@ -0,0 +1,14 @@
+# Controll flow with if else conditional operators: 
+print("Welcome to the roller coastet !!")
+
+Height = int(input("what is your height in cm ? "))
+
+if Height >= 120:
+    print("you can ride the roller coaster.! ")
+
+else:
+    print("sorry you can't ride")
+
+
+
+
